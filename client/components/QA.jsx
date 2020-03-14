625255e99e612356c3e6f51b5d921c0975df89f5
Jeremy Sim <jsim0809@gmail.com>
2020-03-14 10:43:13 -0700

    Branch finished, moving to new feature branch.

---
+++ client/components/QA.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class QA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div id="fullblock">
+        <div id="title">Questions & Answers</div>
+      </div>
+    );
+  }
+}
+
+export default QA;
