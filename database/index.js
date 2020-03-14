5bae82fde7698ef3b04c533b4524d526206fe812
Jeremy Sim <jsim0809@gmail.com>
2020-03-13 20:28:15 -0700

    Sets up very basic Jest functionality. Does not yet work with JSX files.

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/hr-fec-q-a', { useNewUrlParser: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Database connection error:'));
 db.once('open', () => {
   console.log('Connection established with database "hr-fec-q-a".');
 });
