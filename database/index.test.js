1b8b485715272c74bb9601d2722e79ed863808cf
Jeremy Sim <jsim0809@gmail.com>
2020-03-14 08:56:50 -0700

    Fixes merge conflict in database/index.test.js

--- database/index.test.js
+++ database/index.test.js
@@ -2,8 +2,4 @@ const Question = require('./index');
 
 test('checks that the Question document exists', () => {
   expect(Question).toBeDefined();
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> fab532001424852e8e02f0aec5da9f7ecb6e9578
