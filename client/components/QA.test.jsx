a3bd176bfe8aa90d0a0ed1ed03613c08399e6d96
Jeremy Sim <jsim0809@gmail.com>
2020-03-15 08:59:00 -0700

    It... just works!?

---
+++ client/components/QA.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import QA from './QA';
+
+
+it('renders correctly', () => {
+  const wrapper = Enzyme.shallow(<QA />);
+
+  expect(wrapper.length).toBe(1);
+  expect(wrapper.text()).toBe('Questions & Answers');
+});
