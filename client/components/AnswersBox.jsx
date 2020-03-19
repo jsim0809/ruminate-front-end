c602ff1bbd27b9a39e326f3f18c136e3776475a4
Jeremy Sim <jsim0809@gmail.com>
2020-03-19 06:27:02 -0700

    Implements flag tooltips.

--- client/components/AnswersBox.jsx
+++ client/components/AnswersBox.jsx
@@ -11,7 +11,7 @@ const AnswersBox = ({ extendAnswers, question, handleLoginClick }) => {
             <div className="answer-left-line">
               <VoteSwitch votes={answer.votes} handleLoginClick={handleLoginClick} />
               <div className="answer-header">
-                Response from {answer.author.username} | Reviewed this property | <img className="large-flag" src="./images/flag-icon.png" onClick={handleLoginClick} />
+                Response from {answer.author.username} | Reviewed this property | <span><img className="large-flag" src="./images/flag-icon.png" onClick={handleLoginClick} /><span className="flag-tooltip">Problem with this answer?</span></span>
               </div>
               <div className="answer-text">
                 {answer.text}
@@ -28,7 +28,7 @@ const AnswersBox = ({ extendAnswers, question, handleLoginClick }) => {
       <div className="answer-left-line">
         <VoteSwitch votes={question.answers[0].votes} handleLoginClick={handleLoginClick} />
         <div className="answer-header">
-          Response from {question.answers[0].author.username} | Reviewed this property | <img className="large-flag" src="./images/flag-icon.png" onClick={handleLoginClick} />
+          Response from {question.answers[0].author.username} | Reviewed this property | <span><img className="large-flag" src="./images/flag-icon.png" onClick={handleLoginClick} /><span className="flag-tooltip">Problem with this answer?</span></span>
         </div>
         <div className="answer-text">
           {question.answers[0].text}
