b9cd0f49ca5c4ffea7ee8be58a43d0e887c4ea96
Jeremy Sim <jsim0809@gmail.com>
2020-03-19 00:55:53 -0700

    Removes some Jest tests for now

--- client/components/AnswerForm.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import AnswerForm from './AnswerForm';
-import GuidelinesModal from './GuidelinesModal';
-
-it('contains all the correct components', () => {
-  const wrap = Enzyme.shallow(<AnswerForm />);
-  expect(wrap.exists());
-});
--- client/components/QsAndAsBox.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import QsAndAsBox from './QsAndAsBox';
-
-it('contains all the correct components', () => {
-  const wrap = Enzyme.shallow(<QsAndAsBox />);
-  expect(wrap.exists());
-});
