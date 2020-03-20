900a6240fba4ffaa88e40b923dacf978a8653a4c
Jeremy Sim <jsim0809@gmail.com>
2020-03-20 03:13:25 -0700

    Removed a leftover test file.

--- client/components/Headshot.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import Headshot from './Headshot';
-
-it('contains all the correct components', () => {
-  const wrap = Enzyme.shallow(<Headshot />);
-  expect(wrap.exists());
-});
